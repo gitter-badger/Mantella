4a28815c1ae2a2c18b40beade43c5c2e029b119f
dkiechle <kiechle@sra.uni-hannover.de>
2015-10-14 11:43:51 +0200

    added exception tests for HookeJeevesAlgorithm

--- test/optimisationAlgorithm/trajectoryBasedOptimisationAlgorithm/testHookeJeevesAlgorithm.cpp
+++ test/optimisationAlgorithm/trajectoryBasedOptimisationAlgorithm/testHookeJeevesAlgorithm.cpp
@@ -9,16 +9,59 @@
 
 TEST_CASE(
     "HookeJeevesAlgorithm") {
+  std::shared_ptr<mant::OptimisationProblem> optimisationProblem(new mant::bbob::SphereFunction(2));
+  mant::HookeJeevesAlgorithm hookeJeevesAlgorithm(optimisationProblem);
+  hookeJeevesAlgorithm.setMaximalNumberOfIterations(10000);
+
   SECTION(
       ".optimise") {
     // TODO
+  }
+
+  SECTION("Exception tests") {
+
+    SECTION(
+          "Throws an exception, if the InitialStepSize zero" ) {
+      CHECK_THROWS_AS(hookeJeevesAlgorithm.setInitialStepSize({0, 0}), std::logic_error);
+    }
+
+    SECTION(
+          "Throws an exception, if the InitialStepSize lower than zero" ) {
+      CHECK_THROWS_AS(hookeJeevesAlgorithm.setInitialStepSize({-0.001, -8.5}), std::logic_error);
+    }
+
+    SECTION(
+          "Throws an exception, if the size of InitialStepSize is not equal to the number of dimension of the problem" ) {
+      CHECK_THROWS_AS(hookeJeevesAlgorithm.setInitialStepSize(arma::randu<arma::Mat<double>>(std::uniform_int_distribution<arma::uword>(3, 10)(mant::Rng::getGenerator())) * 200 - 100), std::logic_error);
+      CHECK_THROWS_AS(hookeJeevesAlgorithm.setInitialStepSize(arma::randu<arma::Mat<double>>(1) * 200 - 100), std::logic_error);
+    }
+
+    SECTION(
+          "Throws an exception, if the StepSizeDecrease lower than zero" ) {
+      CHECK_THROWS_AS(hookeJeevesAlgorithm.setStepSizeDecrease({-0.001, -8.5}), std::logic_error);
+    }
+
+    SECTION(
+          "Throws an exception, if the StepSizeDecrease higher than one" ) {
+      CHECK_THROWS_AS(hookeJeevesAlgorithm.setStepSizeDecrease({1.8, 8.5}), std::logic_error);
+    }
+
+    SECTION(
+          "Throws an exception, if the StepSizeDecrease zero" ) {
+      CHECK_THROWS_AS(hookeJeevesAlgorithm.setStepSizeDecrease({0.0, 0.0}), std::logic_error);
+    }
+
+    SECTION(
+          "Throws not an exception, if the StepSizeDecrease one" ) {
+      CHECK_NOTHROW(hookeJeevesAlgorithm.setStepSizeDecrease({1.0, 1.0}));
+
+    }
   }
 
   SECTION(
       ".toString") {
     SECTION(
         "Returns the expected class name.") {
-      std::shared_ptr<mant::OptimisationProblem> optimisationProblem(new mant::bbob::SphereFunction(2));
       CHECK(mant::HookeJeevesAlgorithm(optimisationProblem).toString() ==
             "hooke_jeeves_algorithm");
     }
